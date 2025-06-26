5b005d190079e44d21465c1bf3b1f07026ffddff
Aniruddh Dubey <justani98@gmail.com>
2025-06-26 17:23:54 +0530

    Remove unused components

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,6 +1,11 @@
-import PaperExamples from '@/components/PaperExamples';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
 import React from 'react';
 
 export default function TabTwoScreen() {
-  return <PaperExamples />;
+  return (
+    <ThemedView style={{ flex: 1, justifyContent: 'center', alignItems: 'center', padding: 20 }}>
+      <ThemedText type="title">Flashcards coming soon</ThemedText>
+    </ThemedView>
+  );
 }
